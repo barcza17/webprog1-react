c7efff004f332819f344d2c6f9c735795fc2d151
barcza17 <bar.m2299@gmail.com>
2025-04-16 01:23:26 +0200

    7. részállapot - routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,20 +1,19 @@
-import Home from "./routes/home";
-import App1 from "./routes/app1";
-import App2 from "./routes/app2";
+
 
 const routes = [
   {
     path: "/",
-    Component: Home,
+    file: "./home.tsx",
   },
   {
     path: "/app1",
-    Component: App1,
+    file: "./app1.tsx",
   },
   {
     path: "/app2",
-    Component: App2,
+    file: "./app2.tsx",
   },
 ];
 
 export default routes;
+
